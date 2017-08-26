0e92e045fa150f06930edc588d041d00e9bde62d
codemansam <sam@stacked.co.nz>
2017-08-26 14:42:32 +1200

    Added function to create markers from the marker variables

--- app/js/app.js
+++ app/js/app.js
@@ -205,4 +205,15 @@ window.onload = function() {
 
 	initMap();	
 }
+// Function creates markers with a color parameter.
 
+function makeMarkerIcon(markerColor) {
+var markerImage = new google.maps.MarkerImage(
+  'http://chart.googleapis.com/chart?chst=d_map_spin&chld=1.15|0|'+ markerColor +
+  '|40|_|%E2%80%A2',
+  new google.maps.Size(21, 34),
+  new google.maps.Point(0, 0),
+  new google.maps.Point(10, 34),
+  new google.maps.Size(21,34));
+return markerImage;
+}
