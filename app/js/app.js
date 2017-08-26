f97a54814f44372cd5fe4306f92a9c9291f24aea
codemansam <sam@stacked.co.nz>
2017-08-26 12:40:06 +1200

    Set default zoom to 16

--- app/js/app.js
+++ app/js/app.js
@@ -179,7 +179,7 @@ var styles =
 function initMap() {
 	map = new google.maps.Map(document.getElementById('map'), {
 	  center: {lat: -36.7930251, lng: 174.6784522},
-	  zoom: 15,
+	  zoom: 16,
 	  styles: styles,
 	  mapTypeControl: false, // I don't want users to be able to mess with this.
 	  zoomControl: false
